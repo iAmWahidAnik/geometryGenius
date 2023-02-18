1fc4001136da5ad1abe20c7cea05dc4c5b435f7e
wahidanik <wahid.anik@outlook.com>
2023-02-18 15:30:50 +0600

    all kind of requirments added

--- js/hello.js
+++ js/hello.js
@@ -1,29 +1,38 @@
+// get value from input field as string and convert them to a number 
 function takeInputMakeNumber(elementId){
     const getElement = document.getElementById(elementId);
     const elementString = getElement.value;
     const elementNumber = parseFloat(elementString);
     return elementNumber;
 }
+// get element and set the new value 
 function getElementSetValue(elementId, value){
     const getElement = document.getElementById(elementId);
     getElement.innerText = value;
 }
 // validation function 
-// function validation(inputOne, inputTwo){
-//     if (inputOne <= 0 || inputTwo <= 0 || isNaN(inputOne) || isNaN(inputTwo)){
-//         alert ('please write the values correctly')
-//         return;
-//     }
-// }
+function validation(inputOne, inputTwo){
+    if (inputOne <= 0 || inputTwo <= 0 || isNaN(inputOne) || isNaN(inputTwo)){
+        alert ('please write the values correctly')
+        return true;
+    }
+}
+// color change function 
+function changeColor(elementId){
+    const card = document.getElementById(elementId);
+    card.style.backgroundColor = 'rgb('+ Math.round(Math.random()* 255) + ','+ Math.round(Math.random()* 255) + ',' + Math.round(Math.random()* 255)+')';
+}
+// blog page 
+document.getElementById('btn-blog').addEventListener('click', function(){
+    window.location.href = 'blog.html';
+})
 // triangle 
 document.getElementById('btn-triangle').addEventListener('click', function(){
     const base = takeInputMakeNumber('input-b-triangle');
     const height = takeInputMakeNumber('input-h-triangle');
-    // validation(base, height);
-    // if (base <= 0 || height <= 0 || isNaN(base) || isNaN(height)){
-    //     alert ('please write the values correctly')
-    //     return;
-    // }
+    if (validation(base, height) == true){
+        return;
+    }
     const area = 0.5 * base * height;
     getElementSetValue('set-area-triangle', area.toFixed(2));
     document.getElementById('area-triangle').classList.remove('hidden');
@@ -32,6 +41,9 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
 document.getElementById('btn-rectangle').addEventListener('click', function(){
     const width = takeInputMakeNumber('input-w-rectangle');
     const length = takeInputMakeNumber('input-l-rectangle');
+    if (validation(width, length) == true){
+        return;
+    }
     const area = width * length;
     getElementSetValue('set-area-rectangle', area.toFixed(2));
     document.getElementById('area-rectangle').classList.remove('hidden');
@@ -40,6 +52,9 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
 document.getElementById('btn-Parallelogram').addEventListener('click', function(){
     const base = takeInputMakeNumber('input-b-parallel');
     const height = takeInputMakeNumber('input-h-parallel');
+    if (validation(base, height) == true){
+        return;
+    }
     const area = base * height;
     getElementSetValue('set-area-parallel', area.toFixed(2));
     document.getElementById('area-parallel').classList.remove('hidden');
@@ -48,6 +63,9 @@ document.getElementById('btn-Parallelogram').addEventListener('click', function(
 document.getElementById('btn-rhombus').addEventListener('click', function(){
     const dOne = takeInputMakeNumber('input-d1-rhombus');
     const dTwo = takeInputMakeNumber('input-d2-rhombus');
+    if (validation(dOne, dTwo) == true){
+        return;
+    }
     const area = 0.5 * dOne * dTwo;
     getElementSetValue('set-area-rhombus', area.toFixed(2));
     document.getElementById('area-rhombus').classList.remove('hidden');
@@ -56,6 +74,9 @@ document.getElementById('btn-rhombus').addEventListener('click', function(){
 document.getElementById('btn-pentagon').addEventListener('click', function(){
     const pValue = takeInputMakeNumber('input-p-pentagon');
     const bValue = takeInputMakeNumber('input-b-pentagon');
+    if (validation(pValue, bValue) == true){
+        return;
+    }
     const area = 0.5 * pValue * bValue;
     getElementSetValue('set-area-pentagon', area.toFixed(2));
     document.getElementById('area-pentagon').classList.remove('hidden')
@@ -64,6 +85,9 @@ document.getElementById('btn-pentagon').addEventListener('click', function(){
 document.getElementById('btn-ellipse').addEventListener('click', function(){
     const aValue = takeInputMakeNumber('input-a-ellipse');
     const bValue = takeInputMakeNumber('input-b-ellipse');
+    if (validation(aValue, bValue) == true){
+        return;
+    }
     const area = 3.1415 * aValue * bValue;
     getElementSetValue('set-area-ellipse', area.toFixed(2));
     document.getElementById('area-ellipse').classList.remove('hidden')
